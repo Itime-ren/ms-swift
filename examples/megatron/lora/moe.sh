3eb1806f4c844a15bc670902a5451b117fae4b96
Itime-ren <153210144+Itime-ren@users.noreply.github.com>
2026-01-12 23:46:10 +0800

    Update moe.sh for model training configuration

--- examples/megatron/lora/moe.sh
+++ examples/megatron/lora/moe.sh
@@ -1,45 +1,47 @@
 # 2 * 62GiB, 5.10s/it
 PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True' \
-NPROC_PER_NODE=2 \
-CUDA_VISIBLE_DEVICES=0,1 \
+NPROC_PER_NODE=4 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
 megatron sft \
-    --model Qwen/Qwen3-30B-A3B \
+    --model /root/autodl-tmp/qwen30B_moe \
+    --model_type qwen3_moe \
     --load_safetensors true \
     --save_safetensors true \
     --merge_lora false \
-    --dataset 'swift/Qwen3-SFT-Mixin#2000' \
-              'swift/self-cognition:empty_think#600' \
+    --dataset '/root/autodl-tmp/Qwen/qwen3_finetune_self_cognition.jsonl' \
     --loss_scale ignore_empty_think \
     --train_type lora \
     --lora_rank 8 \
     --lora_alpha 32 \
     --target_modules all-linear \
     --split_dataset_ratio 0.01 \
-    --expert_model_parallel_size 2 \
+    --expert_model_parallel_size 4 \
     --moe_permute_fusion true \
     --moe_grouped_gemm true \
     --moe_shared_expert_overlap true \
     --moe_aux_loss_coeff 1e-3 \
-    --micro_batch_size 8 \
-    --global_batch_size 16 \
+    --micro_batch_size 1 \
+    --global_batch_size 8 \
     --recompute_granularity full \
     --recompute_method uniform \
     --recompute_num_layers 1 \
-    --max_epochs 1 \
+    --max_epochs 100 \
     --finetune true \
     --cross_entropy_loss_fusion true \
     --lr 1e-4 \
     --lr_warmup_fraction 0.05 \
     --min_lr 1e-5 \
     --save megatron_output/Qwen3-30B-A3B \
-    --eval_interval 200 \
-    --save_interval 200 \
+    --eval_interval 2 \
+    --save_interval 20 \
     --max_length 2048 \
     --num_workers 8 \
     --dataset_num_proc 8 \
     --no_save_optim true \
     --no_save_rng true \
     --sequence_parallel true \
-    --attention_backend flash \
+    --attention_backend false \
     --model_author swift \
-    --model_name swift-robot
+    --model_name swift-robot \
+    --logging_dir ./runs/tensorboard_logs \
+    --report_to wandb
